453d2c384fa4a0787c36aa330d72b22f0d9cb803
mahdi yarmohamadi <yarmohamadi677@gmail.com>
2023-12-09 07:18:10 +0330

    edit ...

--- components/Steps/Step03.js
+++ components/Steps/Step03.js
@@ -1,114 +1,116 @@
-import axios from "axios";
-import NextStep from "./NextStep";
-import { useTranslation } from "react-i18next";
-import { useEffect, useState } from "react";
-import { FaTrashAlt } from "react-icons/fa";
-import Stepmodal from "./Stepmodal";
-import { td, th, step03Td, step03Th } from "./Classes/classSteps";
+import axios from 'axios';
+import NextStep from './NextStep';
+import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+import Stepmodal from './Stepmodal';
+import { td, th, step03Td, step03Th } from './Classes/classSteps';
 const Step03 = () => {
-  const [data, setData] = useState([]);
-  const [showFormGrade, setShowFormGrade] = useState(false);
-  useEffect(() => {
-    const fetchStep03 = async () => {
-      await axios
-        .get(`${process.env.NEXT_PUBLIC_URL}/auth/getStep03`)
-        .then(
-          (res) => {
-            setData(res.data.data)
-            console.log(res.data)
-          }
-        ).catch(
-          (err) => {
-            console.log(err)
-          }
-        )
-    }
-    fetchStep03()
-  }, [showFormGrade]);
+	const [data, setData] = useState([]);
+	const [showFormGrade, setShowFormGrade] = useState(false);
+	useEffect(() => {
+		const fetchStep03 = async () => {
+			await axios
+				.get(`${process.env.NEXT_PUBLIC_URL}/auth/getStep03`)
+				.then((res) => {
+					setData(res.data.data);
+					console.log(res.data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		};
+		fetchStep03();
+	}, [showFormGrade]);
 
+	const setModal = () => {
+		setShowFormGrade(!showFormGrade);
+	};
 
-  const setModal = () => {
-    setShowFormGrade(!showFormGrade);
-  };
-
-  const { t } = useTranslation();
-  const fetchData = () => {
-    dispatch(toggleStep(stepform + 1))
-  }
-  const handleDelete = async (itemId) => {
-    try {
-      const response = await axios.delete(`${process.env.NEXT_PUBLIC_URL}/auth/delStep03/${itemId}`);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  return (
-    <div className=" fade-in  h-full w-full">
-      <div className="  vh70 rounded-md m-1 px-8 text-center bg-indigo-900 bg-opacity-60 flex flex-col justify-between">
-        <div className=" w-full">
-          <div className=" py-4 text-3xl text-white font-black">
-            {t("titleStep03")}
-          </div>
-          <div>
-            <div className="border border-indigo-50 h-full w-full overflow-auto">
-              <table className=" w-full min-w-max table-auto text-left">
-                <thead className=" ">
-                  <tr>
-                    <th className={step03Th}></th>
-                    <th className={step03Th}>{t("grade")}</th>
-                    <th className={step03Th}>{t("start")}</th>
-                    <th className={step03Th}>{t("end")}</th>
-                    <th className={step03Th}>{t("fieldOfStudy")}</th>
-                    <th className={step03Th}>{t("country")}</th>
-                    <th className={step03Th}>{t("city")}</th>
-                    <th className={step03Th}>{t("schoolOrUnivercityName")}</th>
-                    <th className={step03Th}>{t("GPA")}</th>
-                    <th className={step03Th}>{t("outOf")}</th>
-                    <th className={step03Th}>{t("fileGrade")}</th>
-                  </tr>
-                </thead>
-                {data.map((item) => (
-                  <tbody key={item.id} className="w-full">
-                    <tr>
-                      <td className={step03Td}><button onClick={() => handleDelete(item.id)}><FaTrashAlt className=" h-6 text-rose-800" /></button></td>
-                      <td className={step03Td}>{item.grade}</td>
-                      <td className={step03Td}>{item.start}</td>
-                      <td className={step03Td}>{item.end}</td>
-                      <td className={step03Td}>{item.fieldOfStudy}</td>
-                      <td className={step03Td}>{item.country}</td>
-                      <td className={step03Td}>{item.city}</td>
-                      <td className={step03Td}>{item.schoolOrUnivercityName}</td>
-                      <td className={step03Td}>{item.GPA}</td>
-                      <td className={step03Td}>{item.outOf}</td>
-                      <td className={step03Td}>{item.fileGrade}</td>
-                    </tr>
-                  </tbody>
-                ))}
-              </table>
-            </div>
-          </div>
-          <div>
-            <button
-              onClick={() => {
-                setShowFormGrade(true);
-              }}
-              className=" shadow-md bg-indigo-900 hover:bg-indigo-800 p-2 text-lg text-indigo-50 font-extrabold w-full rounded-md border border-indigo-50 hover:border-indigo-950 my-5"
-              type="button"
-            >
-              {t("clickToRegisterDegree")}
-            </button>
-          </div>
-        </div>
-        {showFormGrade && (
-          <Stepmodal
-            setModal={setModal}
-            title={"recordRegisterDegree"}
-          />
-        )}
-        <NextStep disableForm={data.length < 1} type={"submit"} onClick={fetchData} />
-      </div>
-    </div>
-  );
+	const { t } = useTranslation();
+	const fetchData = () => {
+		dispatch(toggleStep(stepform + 1));
+	};
+	const handleDelete = async (itemId) => {
+		try {
+			const response = await axios.delete(
+				`${process.env.NEXT_PUBLIC_URL}/auth/delStep03/${itemId}`
+			);
+			console.log(response.data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+	return (
+		<div className=' fade-in  h-full w-full'>
+			<div className='  vh70 rounded-md m-1 px-8 text-center bg-indigo-900 bg-opacity-60 flex flex-col justify-between'>
+				<div className=' w-full'>
+					<div className=' py-4 text-3xl text-white font-black'>{t('titleStep03')}</div>
+					<div>
+						<div className='border border-indigo-50 h-full w-full overflow-auto'>
+							<table className=' w-full min-w-max table-auto text-left'>
+								<thead className=' '>
+									<tr>
+										<th className={step03Th}></th>
+										<th className={step03Th}>{t('grade')}</th>
+										<th className={step03Th}>{t('start')}</th>
+										<th className={step03Th}>{t('end')}</th>
+										<th className={step03Th}>{t('fieldOfStudy')}</th>
+										<th className={step03Th}>{t('country')}</th>
+										<th className={step03Th}>{t('city')}</th>
+										<th className={step03Th}>{t('schoolOrUnivercityName')}</th>
+										<th className={step03Th}>{t('GPA')}</th>
+										<th className={step03Th}>{t('outOf')}</th>
+										<th className={step03Th}>{t('fileGrade')}</th>
+									</tr>
+								</thead>
+								{data.map((item) => (
+									<tbody key={item.id} className='w-full'>
+										<tr>
+											<td className={step03Td}>
+												<button onClick={() => handleDelete(item.id)}>
+													<FaTrashAlt className=' h-6 text-rose-800' />
+												</button>
+											</td>
+											<td className={step03Td}>{item.grade}</td>
+											<td className={step03Td}>{item.start}</td>
+											<td className={step03Td}>{item.end}</td>
+											<td className={step03Td}>{item.fieldOfStudy}</td>
+											<td className={step03Td}>{item.country}</td>
+											<td className={step03Td}>{item.city}</td>
+											<td className={step03Td}>
+												{item.schoolOrUnivercityName}
+											</td>
+											<td className={step03Td}>{item.GPA}</td>
+											<td className={step03Td}>{item.outOf}</td>
+											<td className={step03Td}>
+												<img
+													src={`${process.env.NEXT_PUBLIC_URL}/${item.fileGrade}`}
+													alt=''
+												/>
+											</td>
+										</tr>
+									</tbody>
+								))}
+							</table>
+						</div>
+					</div>
+					<div>
+						<button
+							onClick={() => {
+								setShowFormGrade(true);
+							}}
+							className=' shadow-md bg-indigo-900 hover:bg-indigo-800 p-2 text-lg text-indigo-50 font-extrabold w-full rounded-md border border-indigo-50 hover:border-indigo-950 my-5'
+							type='button'
+						>
+							{t('clickToRegisterDegree')}
+						</button>
+					</div>
+				</div>
+				{showFormGrade && <Stepmodal setModal={setModal} title={'recordRegisterDegree'} />}
+				<NextStep disableForm={data.length < 1} type={'submit'} onClick={fetchData} />
+			</div>
+		</div>
+	);
 };
 export default Step03;
--- components/Steps/Stepmodal.js
+++ components/Steps/Stepmodal.js
@@ -1,154 +1,152 @@
-import React, { useState } from "react";
-import { useFormik } from "formik";
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
 
-import { FaRegTimesCircle } from "react-icons/fa";
-import { useTranslation } from "react-i18next";
-import SelectForm from "./FormSteps/SelectForm";
-import Inputform from "./FormSteps/Inputform";
-import InputFileform from "./FormSteps/InputFileForm";
-import { Step03Validation } from "@/Validation/formValidate";
-import axios from "axios";
+import { FaRegTimesCircle } from 'react-icons/fa';
+import { useTranslation } from 'react-i18next';
+import SelectForm from './FormSteps/SelectForm';
+import Inputform from './FormSteps/Inputform';
+import InputFileform from './FormSteps/InputFileForm';
+import { Step03Validation } from '@/Validation/formValidate';
+import axios from 'axios';
 
-const nameGrade = [
-  'diploma',
-  'associateDegree',
-  'bachelor',
-  'master',
-  'phd',
-]
+const nameGrade = ['diploma', 'associateDegree', 'bachelor', 'master', 'phd'];
 const initialValues = {
-  grade: "",
-  start: "",
-  end: "",
-  fieldOfStudy: "",
-  country: "",
-  city: "",
-  schoolOrUnivercityName: "",
-  GPA: "",
-  outOf: "",
-  fileGrade: null,
+	grade: '',
+	start: '',
+	end: '',
+	fieldOfStudy: '',
+	country: '',
+	city: '',
+	schoolOrUnivercityName: '',
+	GPA: '',
+	outOf: '',
+	fileGrade: null,
 };
-const validationSchema = Step03Validation
-
+const validationSchema = Step03Validation;
 
 const Stepmodal = ({ setModal, title, disableForm, setShowFormGrade }) => {
-  const { t } = useTranslation();
-  const [file, setFile] = useState("")
-  const [preview, setPreview] = useState("")
+	const { t } = useTranslation();
+	const [file, setFile] = useState('');
+	const [preview, setPreview] = useState('');
 
-  const handlechange = (e) => {
-    const image = e.target.files[0]
-    setPreview(URL.createObjectURL(image))
-    setFile(image)
-    console.log('FILE=', file)
-    console.log(preview)
-  }
-  const onSubmit = async (values) => {
-    const formData = new FormData()
-    formData.append("userId", "2")
-    formData.append("registrationNoticesId", "3")
-    formData.append("grade", values.grade)
-    formData.append("start", values.start)
-    formData.append("end", values.end)
-    formData.append("fieldOfStudy", values.fieldOfStudy)
-    formData.append("country", values.country)
-    formData.append("city", values.city)
-    formData.append("schoolOrUnivercityName", values.schoolOrUnivercityName)
-    formData.append("GPA", values.GPA)
-    formData.append("outOf", values.outOf)
-    formData.append("fileGrade", file)
-    const data = Object.fromEntries(formData)
-    await axios({
-      method: 'post',
-      url: `${process.env.NEXT_PUBLIC_URL}/auth/step03`,
-      data: formData,
-      headers: {
-        'Content-Type': `multipart/form-data`,
-      },
-    })
-      .then((res) => {
-        setModal(false);
-        console.log(res)
-      })
-      .catch((error) => {
-        if (error.response) {
-          // console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) {
-          console.log(error.request);
-        } else {
-          console.log('Error', error.message);
-        }
-        console.log(error.config);
+	const handlechange = (e) => {
+		const image = e.target.files[0];
+		setPreview(URL.createObjectURL(image));
+		setFile(image);
+		console.log('FILE=', file);
+		console.log(preview);
+	};
+	const onSubmit = async (values) => {
+		const formData = new FormData();
+		formData.append('userId', '2');
+		formData.append('registrationNoticesId', '3');
+		formData.append('grade', values.grade);
+		formData.append('start', values.start);
+		formData.append('end', values.end);
+		formData.append('fieldOfStudy', values.fieldOfStudy);
+		formData.append('country', values.country);
+		formData.append('city', values.city);
+		formData.append('schoolOrUnivercityName', values.schoolOrUnivercityName);
+		formData.append('GPA', values.GPA);
+		formData.append('outOf', values.outOf);
+		formData.append('fileGrade', file);
+		const data = Object.fromEntries(formData);
+		console.log(data);
+		console.log(formData);
+		await axios
+			.post(`${process.env.NEXT_PUBLIC_URL}/auth/step03`, formData)
+			.then((res) => {
+				setModal(false);
+				console.log(res);
+			})
+			.catch((error) => {
+				if (error.response) {
+					console.log(error.response.data);
+					console.log(error.response.status);
+					console.log(error.response.headers);
+				} else if (error.request) {
+					console.log(error.request);
+				} else {
+					console.log('Error', error.message);
+				}
+				console.log(error.config);
+			});
+	};
+	const formik = useFormik({
+		initialValues,
+		onSubmit,
+		validationSchema,
+	});
+	// console.log(formik.values)
 
-      });
+	return (
+		<div className=' fade-in  justify-center  flex  overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none backdrop-blur-md'>
+			<div className='relative  w-3/5 '>
+				<div className=' flex flex-col justify-start  px-6 pt-5 mx-auto  lg:pt-10'>
+					<div className=' text-start bg-indigo-50  shadow-lg shadow-indigo-900 w-full  rounded-lg  dark:border md:mt-0  xl:p-0 dark:bg-indigo-800 dark:border-indigo-700'>
+						<div className='p-1'>
+							<form
+								onSubmit={formik.handleSubmit}
+								enctype='multipart/form-data'
+								className='bg-indigo-900 bg-opacity-50 p-4 m-2 border border-indigo-950 rounded-lg shadow-md '
+							>
+								<div className=' text-end'>
+									<button className='' type='' onClick={setModal}>
+										<FaRegTimesCircle
+											className='text-indigo-50  m-1 h-6 w-6 '
+											aria-hidden='true'
+										/>
+									</button>
+								</div>
 
-  };
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema,
-  });
-  // console.log(formik.values)
+								<div className='text-center'>
+									<h1 className='pb-8 px-8 -mt-7  text-xl font-extrabold leading-tight tracking-tight text-indigo-900 md:text-2xl dark:text-white '>
+										{t(title)}
+									</h1>
+								</div>
 
-  return (
-    <div className=" fade-in  justify-center  flex  overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none backdrop-blur-md">
-      <div className="relative  w-3/5 ">
-        <div className=" flex flex-col justify-start  px-6 pt-5 mx-auto  lg:pt-10">
-          <div className=" text-start bg-indigo-50  shadow-lg shadow-indigo-900 w-full  rounded-lg  dark:border md:mt-0  xl:p-0 dark:bg-indigo-800 dark:border-indigo-700">
-            <div className="p-1">
-              <form
-                onSubmit={formik.handleSubmit}
-                enctype="multipart/form-data"
-                className="bg-indigo-900 bg-opacity-50 p-4 m-2 border border-indigo-950 rounded-lg shadow-md "
-              >
-                <div className=" text-end">
-                  <button className="" type="" onClick={setModal}>
-                    <FaRegTimesCircle
-                      className="text-indigo-50  m-1 h-6 w-6 "
-                      aria-hidden="true"
-                    />
-                  </button>
-                </div>
-
-                <div className="text-center">
-                  <h1 className="pb-8 px-8 -mt-7  text-xl font-extrabold leading-tight tracking-tight text-indigo-900 md:text-2xl dark:text-white ">
-                    {t(title)}
-                  </h1>
-                </div>
-
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
-                  <SelectForm formik={formik} name={'grade'} value={nameGrade} />
-                  <Inputform formik={formik} name={'start'} type={'date'} />
-                  <Inputform formik={formik} name={'end'} type={'date'} />
-                  <Inputform formik={formik} name={'fieldOfStudy'} type={'text'} />
-                  <Inputform formik={formik} name={'country'} type={'text'} />
-                  <Inputform formik={formik} name={'city'} type={'text'} />
-                  <Inputform formik={formik} name={'schoolOrUnivercityName'} type={'text'} />
-                  <Inputform formik={formik} name={'GPA'} type={'number'} />
-                  <Inputform formik={formik} name={'outOf'} type={'number'} />
-                  <InputFileform formik={formik} name={'fileGrade'} type={'file'} handlechange={handlechange} />
-                  <div>
-
-                  </div>
-                </div>
-                <div>
-                  <button
-                    className="w-full p-2 my-3 rounded-md bg-indigo-800 hover:bg-indigo-900 text-indigo-50 text-lg"
-                    type="submit"
-                    disabled={disableForm}
-                  >
-                    {t("submit")}
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+								<div className='grid grid-cols-1 md:grid-cols-2 gap-4 '>
+									<SelectForm formik={formik} name={'grade'} value={nameGrade} />
+									<Inputform formik={formik} name={'start'} type={'date'} />
+									<Inputform formik={formik} name={'end'} type={'date'} />
+									<Inputform
+										formik={formik}
+										name={'fieldOfStudy'}
+										type={'text'}
+									/>
+									<Inputform formik={formik} name={'country'} type={'text'} />
+									<Inputform formik={formik} name={'city'} type={'text'} />
+									<Inputform
+										formik={formik}
+										name={'schoolOrUnivercityName'}
+										type={'text'}
+									/>
+									<Inputform formik={formik} name={'GPA'} type={'number'} />
+									<Inputform formik={formik} name={'outOf'} type={'number'} />
+									<InputFileform
+										formik={formik}
+										name={'fileGrade'}
+										type={'file'}
+										handlechange={handlechange}
+									/>
+									<div></div>
+								</div>
+								<div>
+									<button
+										className='w-full p-2 my-3 rounded-md bg-indigo-800 hover:bg-indigo-900 text-indigo-50 text-lg'
+										type='submit'
+										disabled={disableForm}
+									>
+										{t('submit')}
+									</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Stepmodal;
